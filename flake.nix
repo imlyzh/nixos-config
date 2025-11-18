2b2622c40288009d2045f12dd8a44af9683cb630
imlyzh <enterindex@gmail.com>
2025-11-19 07:46:41 +0800

    update home-manager config flake

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
 
-    home-manager.url = "github:nix-community/home-manager";
+    home-manager.url = "github:nix-community/home-manager/release-25.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     
     rust-overlay = {
