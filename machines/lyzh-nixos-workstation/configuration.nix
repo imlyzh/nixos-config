cc579bb85e7da03e749c7086b4781fd6a6111fc2
imlyzh <enterindex@gmail.com>
2026-01-02 14:03:25 +0800

    nix flake update and config im

--- machines/lyzh-nixos-workstation/configuration.nix
+++ machines/lyzh-nixos-workstation/configuration.nix
@@ -58,11 +58,9 @@
     type = "fcitx5";
     fcitx5.addons = with pkgs; [
       fcitx5-rime
-      # fcitx5-chinese-addons
       qt6Packages.fcitx5-chinese-addons
-      fcitx5-gtk
-      # fcitx5-configtool
       qt6Packages.fcitx5-configtool
+      fcitx5-gtk
     ];
   };
 
@@ -197,11 +195,12 @@
     CC = "clang";
     CXX = "clang++";
 
-    # GTK_IM_MODULE = "ibus";
-    # QT_IM_MODULE = "ibus";
-    # XMODIFIERS = "@im=ibus";
-    # INPUT_METHOD = "ibus";
-    # SDL_IM_MODULE = "ibus";
+    GTK_IM_MODULE = "fcitx";
+    QT_IM_MODULE = "fcitx";
+    XMODIFIERS = "@im=fcitx";
+    INPUT_METHOD = "fcitx";
+    SDL_IM_MODULE = "fcitx";
+    GLFW_IM_MODULE = "ibus";
   };
 
   # services.blueman.enable = true;
