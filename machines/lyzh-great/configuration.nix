9c304c91eb0780ba47a5917c557cd7184856d0cc
imlyzh <enterindex@gmail.com>
2026-02-08 19:47:28 +0800

    feat(i18n): fix ibus input method configuration syntax
    The i18n.inputMethod configuration was using the deprecated 'enabled' key
    instead of the correct 'enable' key. Changed 'enabled = "ibus"' to
    'enable = true' and added 'type = "ibus"' to properly configure the
    IBus input method with Rime engine support.

--- machines/lyzh-great/configuration.nix
+++ machines/lyzh-great/configuration.nix
@@ -51,7 +51,8 @@
     LC_TIME = "zh_CN.UTF-8";
   };
   i18n.inputMethod = {
-    enabled = "ibus";
+    enable = true;
+    type = "ibus";
     ibus.engines = with pkgs.ibus-engines; [
       rime
     ];
