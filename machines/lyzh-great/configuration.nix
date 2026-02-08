783c761a421d3aa52c284b1a92668dee43efd509
imlyzh <enterindex@gmail.com>
2026-02-09 00:19:31 +0800

    feat(gaming): enable gamescope and mangohud for enhanced gaming experience
    Adds gamescope and mangohud to the system configuration to provide improved
    gaming performance monitoring and window compositing capabilities. These tools
    are commonly used alongside Steam and gamemode for a better gaming setup.

--- machines/lyzh-great/configuration.nix
+++ machines/lyzh-great/configuration.nix
@@ -201,6 +201,8 @@
   programs.thunar.enable = true;
   programs.steam.enable = true;
   programs.gamemode.enable = true;
+  programs.gamescope.enable = true;
+  programs.mangohud.enable = true;
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
