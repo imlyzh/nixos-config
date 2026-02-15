deb7a89899e30aed54f7dfeea3ebce7c6e92fb8d
imlyzh <enterindex@gmail.com>
2026-02-15 15:21:38 +0800

    feat(kernel): adjust kernel modules and parameters for lyzh-great
    - Remove `hp-wmi` from `boot.kernelModules` as it may not be needed or could cause issues.
    - Add ACPI OSI strings to `boot.kernelParams` to improve hardware compatibility, particularly for hybrid graphics support.

--- machines/lyzh-great/configuration.nix
+++ machines/lyzh-great/configuration.nix
@@ -21,8 +21,8 @@
   boot.kernelPackages = pkgs.linuxPackages_latest;
   # boot.kernelPackages = pkgs.linuxPackages_zen;
   boot.supportedFilesystems = [ "btrfs" ];
-  boot.kernelModules = [ "intel_hfi" "xe" "tun" "hp-wmi" ];
-  boot.kernelParams = [ "intel_pstate=active" "usbcore.autosuspend=-1" ];
+  boot.kernelModules = [ "intel_hfi" "xe" "tun" ];
+  boot.kernelParams = [ "intel_pstate=active" "usbcore.autosuspend=-1" "acpi_osi=!" "acpi_osi=Linux" "acpi_osi=Linux-HPI-Hybrid-Graphics" ];
 
   networking.hostName = "lyzh-great"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
