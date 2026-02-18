a9bb060800cba39529fce28812871adc0cc84130
imlyzh <enterindex@gmail.com>
2026-02-18 16:58:44 +0800

    feat(kernel): add intel_hfi and threadirqs boot parameters
    Add `intel_hfi=on` and `threadirqs` to the kernel boot parameters to enable Intel Hardware Feedback Interface and threaded interrupt handling, improving power efficiency and system responsiveness.

--- machines/lyzh-great/configuration.nix
+++ machines/lyzh-great/configuration.nix
@@ -22,7 +22,7 @@
   # boot.kernelPackages = pkgs.linuxPackages_zen;
   boot.supportedFilesystems = [ "btrfs" ];
   boot.kernelModules = [ "intel_hfi" "xe" "tun" "tcp_bbr" ];
-  boot.kernelParams = [ "intel_pstate=active" "usbcore.autosuspend=-1" ];
+  boot.kernelParams = [ "intel_pstate=active" "intel_hfi=on" "threadirqs" "usbcore.autosuspend=-1" ];
   boot.kernel.sysctl = {
     "net.core.default_qdisc" = "fq_codel";
     "net.ipv4.tcp_congestion_control" = "bbr";
