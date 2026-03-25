d7c75b40d8a52cb48974b91d67b57a304bd54be7
imlyzh <enterindex@gmail.com>
2026-03-26 02:38:24 +0800

    feat: disable SCX scheduler and comment out IRQ balance config
    - Disable SCX scheduler by setting `services.scx.enable` to false
    - Comment out `services.irqbalance.enable` line instead of setting to false
    - Remove commented TCP buffer sysctl configurations for cleaner code

--- machines/lyzh-great/configuration.nix
+++ machines/lyzh-great/configuration.nix
@@ -32,10 +32,6 @@
     
   ];
   boot.kernel.sysctl = {
-    # "net.core.rmem_max" = 16777216;
-    # "net.core.wmem_max" = 16777216;
-    # "net.ipv4.tcp_rmem" = "4096 87380 16777216";
-    # "net.ipv4.tcp_wmem" = "4096 65536 16777216";
     "net.ipv4.tcp_congestion_control" = "bbr";
     "net.core.default_qdisc" = "fq";
     "vm.max_map_count" = 2147483642;
@@ -119,9 +115,9 @@
   # '';
   # powerManagement.enable = true;
 
-  services.scx.enable = true;
+  services.scx.enable = false;
   services.scx.scheduler = "scx_lavd";
-  services.irqbalance.enable = false;
+  # services.irqbalance.enable = false;
   # services.thermald.enable = false;
   services.power-profiles-daemon.enable = true;
   # services.auto-cpufreq.enable = true;
